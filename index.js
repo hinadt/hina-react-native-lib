69e943954487374b7140a351f39b4a9df9884a59
王建华 <wangjianhua@hinadt.com>
2023-04-06 17:47:21 +0800

    init project

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNHinaReactNativeLib } = NativeModules;
+
+export default RNHinaReactNativeLib;
---
+++ ios/RNHinaReactNativeLib.m
@@ -0,0 +1,13 @@
+
+#import "RNHinaReactNativeLib.h"
+
+@implementation RNHinaReactNativeLib
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
